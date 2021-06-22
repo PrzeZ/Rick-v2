9a7f6f011a4171f432cbfdf14dbf4e3826664dbd
PrzeZ <przemyslawzykubek@gmail.com>
2021-06-22 17:43:06 +0200

    Initial commit

---
+++ Rick v2/Rick v2.cpp
@@ -0,0 +1,12 @@
+﻿#include <windows.h>
+#include <shellapi.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Get RickRolled m8 :P" << std::endl;
+    ShellExecute(0, 0, L"https://www.youtube.com/watch?v=dQw4w9WgXcQ", 0, 0, SW_SHOW);
+
+    getchar();
+    return 0;
+}
